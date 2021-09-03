bce24d023c63df067cb8cae426d5ecfc5f0fbf8b
Christopher_Madrid <chrismadrid173@gmail.com>
2021-09-03 10:46:03 -0500

    Can now readout different pixels

--- binaryFunctions.py
+++ binaryFunctions.py
@@ -1,3 +1,4 @@
+
 def add_binary(a, b, shift=8):
     return a<<shift | b
 
@@ -13,3 +14,99 @@ def set_bit(num, idx, value):
 def get_bit(num, idx):
     return int(num & (1 << idx) != 0)
 
+def getAllThresholdsType0(reg1Address, reg2Address):
+    #000000XX XXXXXXXX
+    #00000010 00000000
+    allThresholds = []
+    for b in range(0b00, 0b11 + 0b1):
+        for a in range(0b00, 0b11111111 + 0b1):
+            threshold = add_binary(b, a)
+            allThresholds.append(dict(binary=get_binary_string(threshold, 16), reg2=b, reg1=a, reg2Address=reg2Address, reg1Address=reg1Address))
+    return allThresholds
+
+def getAllThresholdsType1(reg1Address, reg2Address):
+    #0000XXXX XXXXXX10
+    #00001000 00000010
+    constant = 0b10
+    allThresholds = []
+    for b in range(0b00, 0b1111 + 0b1):
+        for a in range(0b00, 0b111111 + 0b1):
+            c = add_binary(a, constant, 2)
+            threshold = add_binary(b, a)
+            total = add_binary(b, c)
+            allThresholds.append(dict(binary=get_binary_string(total, 16), reg2=b, reg1=c, reg2Address=reg2Address, reg1Address=reg1Address))
+    return allThresholds
+
+def getAllThresholdsType2(reg1Address, reg2Address):
+    #00XXXXXX XXXX1000
+    #00100000 00001000
+    constant = 0b1000
+    allThresholds = []
+    for b in range(0b00, 0b111111 + 0b1):
+        for a in range(0b00, 0b1111 + 0b1):
+            c = add_binary(a, constant, 4)
+            threshold = add_binary(b, a)
+            total = add_binary(b, c)
+            allThresholds.append(dict(binary=get_binary_string(total, 16), reg2=b, reg1=c, reg2Address=reg2Address, reg1Address=reg1Address))
+    return allThresholds
+
+def getAllThresholdsType3(reg1Address, reg2Address):
+    #XXXXXXXX XX100000
+    #10000000 00100000
+    constant = 0b100000
+    allThresholds = []
+    for b in range(0b00, 0b11111111 + 0b1):
+        for a in range(0b00, 0b11 + 0b1):
+            c = add_binary(a, constant, 6)
+            threshold = add_binary(b, a)
+            total = add_binary(b, c)
+            allThresholds.append(dict(binary=get_binary_string(total, 16), reg2=b, reg1=c, reg2Address=reg2Address, reg1Address=reg1Address))
+    return allThresholds
+
+class pixelInfo:
+    def __init__(self, pixelNum, reg1AddressTh, reg2AddressTh, getAllThresholds, grid07, q06, q05):
+        self.pixelNum = pixelNum
+        self.reg1AddressTh = reg1AddressTh
+        self.reg2AddressTh = reg2AddressTh
+        self.getAllThresholds = getAllThresholds(reg1AddressTh, reg2AddressTh)
+        self.grid07 = grid07
+        self.q06 = q06
+        self.q05 = q05
+
+
+#def getAllThresholdsPixel00(): return getAllThresholdsType0(0x0A, 0x0B)
+#def getAllThresholdsPixel01(): return getAllThresholdsType1(0x0B, 0x0C)
+#def getAllThresholdsPixel02(): return getAllThresholdsType2(0x0C, 0x0D)
+#def getAllThresholdsPixel03(): return getAllThresholdsType3(0x0D, 0x0E)
+#def getAllThresholdsPixel04(): return getAllThresholdsType0(0x0F, 0x10)
+#def getAllThresholdsPixel05(): return getAllThresholdsType1(0x10, 0x11)
+#def getAllThresholdsPixel06(): return getAllThresholdsType2(0x11, 0x12)
+#def getAllThresholdsPixel07(): return getAllThresholdsType3(0x12, 0x13)
+#def getAllThresholdsPixel08(): return getAllThresholdsType0(0x14, 0x15)
+#def getAllThresholdsPixel09(): return getAllThresholdsType1(0x15, 0x16)
+#def getAllThresholdsPixel10(): return getAllThresholdsType2(0x16, 0x17)
+#def getAllThresholdsPixel11(): return getAllThresholdsType3(0x17, 0x18)
+#def getAllThresholdsPixel12(): return getAllThresholdsType0(0x19, 0x1A)
+#def getAllThresholdsPixel13(): return getAllThresholdsType1(0x1A, 0x1B)
+#def getAllThresholdsPixel14(): return getAllThresholdsType2(0x1B, 0x1C)
+#def getAllThresholdsPixel15(): return getAllThresholdsType3(0x1C, 0x1D)
+
+
+pixels = {
+    0  : pixelInfo( 0, 0x0A, 0x0B, getAllThresholdsType0, 0x01, 0x00, 0x01),
+    1  : pixelInfo( 1, 0x0B, 0x0C, getAllThresholdsType1, 0x02, 0x00, 0x02),
+    2  : pixelInfo( 2, 0x0C, 0x0D, getAllThresholdsType2, 0x04, 0x00, 0x04),
+    3  : pixelInfo( 3, 0x0D, 0x0E, getAllThresholdsType3, 0x08, 0x00, 0x08),
+    4  : pixelInfo( 4, 0x0F, 0x10, getAllThresholdsType0, 0x11, 0x00, 0x10),
+    5  : pixelInfo( 5, 0x10, 0x11, getAllThresholdsType1, 0x12, 0x00, 0x20),
+    6  : pixelInfo( 6, 0x11, 0x12, getAllThresholdsType2, 0x14, 0x00, 0x40),
+    7  : pixelInfo( 7, 0x12, 0x13, getAllThresholdsType3, 0x18, 0x00, 0x80),
+    8  : pixelInfo( 8, 0x14, 0x15, getAllThresholdsType0, 0x21, 0x01, 0x00),
+    9  : pixelInfo( 9, 0x15, 0x16, getAllThresholdsType1, 0x22, 0x02, 0x00),
+    10 : pixelInfo(10, 0x16, 0x17, getAllThresholdsType2, 0x24, 0x04, 0x00),
+    11 : pixelInfo(11, 0x17, 0x18, getAllThresholdsType3, 0x28, 0x08, 0x00),
+    12 : pixelInfo(12, 0x19, 0x1A, getAllThresholdsType0, 0x31, 0x10, 0x00),
+    13 : pixelInfo(13, 0x1A, 0x1B, getAllThresholdsType1, 0x32, 0x20, 0x00),
+    14 : pixelInfo(14, 0x1B, 0x1C, getAllThresholdsType2, 0x34, 0x40, 0x00),
+    15 : pixelInfo(15, 0x1C, 0x1D, getAllThresholdsType3, 0x38, 0x80, 0x00),
+}
--- scanThresholds.py
+++ scanThresholds.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 from etroc_i2c import ETROC_I2C
-from binaryFunctions import add_binary, get_binary_string
+import binaryFunctions as bf
 import os
 import ROOT
 from array import array
@@ -17,43 +17,22 @@ def getAllQinj():
     allQinj = []
     b = 0b111
     for a in range(0b0, 0b11111 + 0b1):
-        c = add_binary(a, b, 3)
-        allQinj.append(dict(binary=get_binary_string(c), regA=c))
+        c = bf.add_binary(a, b, 3)
+        allQinj.append(dict(binary=bf.get_binary_string(c), reg1=c))
     return allQinj
-
-def getAllThresholds():
-    #Get all possible pixel 1 thresholds
-    #000000XX XXXXXXXX
-    allThresholds = []
-    for regB in range(0b00, 0b11 + 0b1):
-        for regA in range(0b00, 0b11111111 + 0b1):
-            threshold = add_binary(regB, regA)
-            allThresholds.append(dict(binary=get_binary_string(threshold, 16), regB=regB, regA=regA))
-    return allThresholds
-
-def getAllThresholdsPixel15():
-    #Get all possible pixel 15 thresholds
-    #XXXXXXXX XX100000
-    constant = 0b100000
-    allThresholds = []
-    for b in range(0b00, 0b11111111 + 0b1):
-        for a in range(0b00, 0b11 + 0b1):
-            c = add_binary(a, constant, 6)
-            threshold = add_binary(b, a)
-            total = add_binary(b, c)
-            allThresholds.append(dict(binary=get_binary_string(total, 16), regB=b, regA=c))
-    return allThresholds
 
 if __name__ == '__main__':
     parser = optparse.OptionParser("usage: %prog [options]\n")
+    parser.add_option('--pixel', dest='pixNum', type='int', default = 0, help="Set which pixel to readout in DMRO mode")
     parser.add_option('--Qinj', dest='Qinj', type='int', default = 0xFF, help="Set Qinj register value")
     parser.add_option('--outfile', dest='outfile', default = "data.root", help="Set output root file name")
-    parser.add_option('--nEvents', dest='nEvents', type='int', default = 1000, help="Set number of events to take per setting")
+    parser.add_option('--nEvents', dest='nEvents', type='int', default = 32000, help="Set number of events to take per setting")
     options, args = parser.parse_args()
 
     Qinj = options.Qinj
     outfile = options.outfile
     nEvents = options.nEvents
+    pixNum = options.pixNum
     doPhaseScan = True
     print("Qinj value: {}".format(hex(Qinj)))
     print("Outfile name: {}".format(outfile))
@@ -75,53 +54,33 @@ if __name__ == '__main__':
     allPhase = getPhaseSettings()
 
     dataTuple = []
-    allThresholds = getAllThresholds()    
-    #allThresholds = getAllThresholdsPixel15()
+    pixelInfo = bf.pixels[pixNum]
+    allThresholds = pixelInfo.getAllThresholds
     for idPhase, phase in enumerate(allPhase):
         if not doPhaseScan and idPhase !=0: break
         if phase != 0x3F: continue
 
         for idx, threshold in reversed(list(enumerate(allThresholds))):
-            #if idx < 360 or idx > 400: continue
-            if idx < 605 or idx > 660: continue
+            #if idx < 250 or idx > 450: continue
+            if idx < 200 or idx > 800: continue
             #if idx != 500: continue
         
-            #Reading out Pixel 1
             commands=[
                 ('w', i2c.r.ETROC_REGB_ADDRESS, 0x00, 0x1C), # default (1C)
                 ('w', i2c.r.ETROC_REGB_ADDRESS, 0x06, 0x41), # disable (40)/enable (41) scrambling
-                #('w', i2c.r.ETROC_REGA_ADDRESS, 0x01, 0x37), # default (37), Q injected amplitude
-                #('w', i2c.r.ETROC_REGA_ADDRESS, 0x01, 0x87), # default (37), Q injected amplitude
-                #('w', i2c.r.ETROC_REGA_ADDRESS, 0x01, 0xCF), # default (37), Q injected amplitude
-                #('w', i2c.r.ETROC_REGA_ADDRESS, 0x01, 0xFF), # default (37), Q injected amplitude
                 ('w', i2c.r.ETROC_REGA_ADDRESS, 0x01, Qinj), # default (37), Q injected amplitude
-                ('w', i2c.r.ETROC_REGA_ADDRESS, 0x0A, threshold['regA']), # default (00), signal threshold pixel 1
-                ('w', i2c.r.ETROC_REGA_ADDRESS, 0x0B, threshold['regB']), # default (02), signal threshold (only first two bits belong to pixel 1 the rest belong to pixel 2)
                 ('w', i2c.r.ETROC_REGB_ADDRESS, 0x04, phase), # default (00), Phase setting
-                ('w', i2c.r.ETROC_REGA_ADDRESS, 0x05, 0x01), ### 01 on, 00 off. disables Q INJ (I2C A REG 05)
-                ('w', i2c.r.ETROC_REGA_ADDRESS, 0x06, 0x00),  ### 00 regardless. disables Q INJ (I2C A REG 06)
                 ('w', i2c.r.ETROC_REGA_ADDRESS, 0x04, 0x00), # default 0x11 
 
-                ('r', i2c.r.ETROC_REGB_ADDRESS, 0x06), # disable (40)/enable (41) scrambling
+                ('w', i2c.r.ETROC_REGA_ADDRESS, threshold['reg1Address'], threshold['reg1']), # default (20), signal threshold pixel 1
+                ('w', i2c.r.ETROC_REGA_ADDRESS, threshold['reg2Address'], threshold['reg2']), # default (80), signal threshold (only first two bits belong to pixel 1 the rest belong to pixel 2)            
+                ('w', i2c.r.ETROC_REGA_ADDRESS, 0x05, pixelInfo.q05), # default (01)
+                ('w', i2c.r.ETROC_REGA_ADDRESS, 0x06, pixelInfo.q06), # default (00)
+                ('w', i2c.r.ETROC_REGA_ADDRESS, 0x07, pixelInfo.grid07), # default (01)
             ]
 
-            ##Reading out Pixel 15
-            #commands=[
-            #    ('w', i2c.r.ETROC_REGB_ADDRESS, 0x00, 0x1C), # default (1C)
-            #    ('w', i2c.r.ETROC_REGB_ADDRESS, 0x06, 0x41), # disable (40)/enable (41) scrambling
-            #    ('w', i2c.r.ETROC_REGA_ADDRESS, 0x01, Qinj), # default (37), Q injected amplitude
-            #    ('w', i2c.r.ETROC_REGA_ADDRESS, 0x05, 0x00), # default (01)
-            #    ('w', i2c.r.ETROC_REGA_ADDRESS, 0x06, 0x80), # default (00)
-            #    ('w', i2c.r.ETROC_REGA_ADDRESS, 0x07, 0x38), # default (01)
-            #    ('w', i2c.r.ETROC_REGA_ADDRESS, 0x1C, threshold['regA']), # default (20), signal threshold pixel 1
-            #    ('w', i2c.r.ETROC_REGA_ADDRESS, 0x1D, threshold['regB']), # default (80), signal threshold (only first two bits belong to pixel 1 the rest belong to pixel 2)
-            #    ('w', i2c.r.ETROC_REGB_ADDRESS, 0x04, 0x00), # default (00), Phase setting
-            #    ('w', i2c.r.ETROC_REGA_ADDRESS, 0x05, 0x01), ### 01 on, 00 off. disables Q INJ (I2C A REG 05)
-            #    ('w', i2c.r.ETROC_REGA_ADDRESS, 0x06, 0x00),  ### 00 regardless. disables Q INJ (I2C A REG 06)
-            #]
-
             i2c.run(commands)
-            print("Setting threshold to {} {} {} {}, Setting phase to {}".format(threshold['binary'], hex(threshold['regB']), hex(threshold['regA']), idx, hex(phase)))
+            print("Setting threshold to {} {}, Setting phase to {} {}".format(threshold['binary'], idx, hex(phase), phase))
 
             #os.system('./run_read_buffer.sh 10000 constant True')
             os.system('./run_read_buffer.sh {} tdc True'.format(nEvents))
